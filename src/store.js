87a3e065ac15a2771c0c8e7dec653d1b0871f6b5
shubham V <Shubham.V@cerner.com>
2023-07-15 20:06:57 +0530

    second arg to createstrore

--- src/store.js
+++ src/store.js
@@ -2,6 +2,17 @@
 import {createStore} from 'redux'
 import rootReducer from './reducer'
 
-const store = createStore(rootReducer);
+//createStore can also accept a preloadedState value as its second argument. You could use this to add initial data when the store is created, such as values that were included in an HTML page sent from the server, or persisted in localStorage and read back when the user visits the page again, like this:
+
+let preloadedState
+const persistedTodosString = localStorage.getItem('todos')
+
+if (persistedTodosString) {
+  preloadedState = {
+    todos: JSON.parse(persistedTodosString)
+  }
+}
+
+const store = createStore(rootReducer, preloadedState)
 
 export default store;
