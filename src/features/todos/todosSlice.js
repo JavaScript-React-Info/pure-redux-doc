6939c1136a0ef85bccf262d48a91e82ebd3c3aa6
shubham V <Shubham.V@cerner.com>
2023-07-16 18:59:39 +0530

    added action creator

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -51,16 +51,39 @@ export default function todosReducer(state = initialState, action) {
   }
 }
 
-// Thunk function
-export async function fetchTodos(dispatch, getState) {
-  const response = await client.get('/fakeApi/todos')
-  dispatch({ type: 'todos/todosLoaded', payload: response.todos })
+export const todosLoaded = todos => {
+  return {
+    type: 'todos/todosLoaded',
+    payload: todos
+  }
 }
+
+export const todoAdded = todo => {
+  return {
+    type: 'todos/todoAdded',
+    payload: todo
+  }
+}
+//export const todoAdded = todo => ({ type: 'todos/todoAdded', payload: todo })
+
+// Thunk function
+export function fetchTodos() {
+  return async function fetchTodosThunk(dispatch, getState) {
+    const response = await client.get('/fakeApi/todos')
+    dispatch(todosLoaded(response.todos))
+  }
+}
+
+// // Same thing as the above example!
+// export const fetchTodos = () => async dispatch => {
+//   const response = await client.get('/fakeApi/todos')
+//   dispatch(todosLoaded(response.todos))
+// }
 
 export function saveNewTodo(text) {
   return async function saveNewTodoThunk(dispatch, getState) {
     const initialTodo = { text }
     const response = await client.post('/fakeApi/todos', { todo: initialTodo })
-    dispatch({ type: 'todos/todoAdded', payload: response.todo })
+    dispatch(todoAdded(response.todo))
   }
 }
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import store from './store';
 import { fetchTodos } from './features/todos/todosSlice'
 
 
-store.dispatch(fetchTodos)
+store.dispatch(fetchTodos())
 
 //Instead, we have to specifically tell React-Redux what store we want to use in our components. We do this by rendering a <Provider> component around our entire <App>, and passing the Redux store as a prop to <Provider>. After we do this once, every component in the application will be able to access the Redux store if it needs to.
 ReactDOM.render(
