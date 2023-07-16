395cf371d8546ddae84cf7f9eaecad3c6b2bf7dd
shubham V <Shubham.V@cerner.com>
2023-07-16 09:15:38 +0530

    configure store with enhancer

--- src/index.js
+++ src/index.js
@@ -1,63 +1,17 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import './index.css'
-// import App from './App'
+import store from "./store";
 
-// import './api/server'
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
+// sayHiOnDispatch: an enhancer that always logs 'Hi'! to the console every time an action is dispatched
+// includeMeaningOfLife: an enhancer that always adds the field meaningOfLife: 42 to the value returned from getState()
 
 
-// Omit existing React imports
 
-import store from './store'
+// console.log('Dispatching actions');
+// store.dispatch({type: 'todos/todoAdded', payload: 'Learn acout actions'});
+// console.log('Dispatch Complete');
 
-// Log the initial state
-console.log('Initial state: ', store.getState())
-// {todos: [....], filters: {status, colors}}
 
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() =>
-  console.log('State after dispatch: ', store.getState())
-)
+store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+// log: 'Hi!'
 
-// Now, dispatch some actions
-
- store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
- store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
-
-store.dispatch({ type: 'todos/todoToggled', payload: 0 })
-store.dispatch({ type: 'todos/todoToggled', payload: 1 })
-
- store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active' })
-
-store.dispatch({
-  type: 'filters/colorFilterChanged',
-  payload: { color: 'red', changeType: 'added' }
-})
-
-// Stop listening to state updates
-unsubscribe()
-
-// Dispatch one more action to see what happens
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
-
-// Omit existing React rendering logic
-
-// Remember, every time we call store.dispatch(action):
-
-//     The store calls rootReducer(state, action)
-//         That root reducer may call other slice reducers inside of itself, like todosReducer(state.todos, action)
-//     The store saves the new state value inside
-//     The store calls all the listener subscription callbacks
-//     If a listener has access to the store, it can now call store.getState() to read the latest state value
-
-// If we look at the console log output from that example, you can see how the Redux state changes as each action was dispatched:
+console.log('State after dispatch: ', store.getState())
+// log: {todos: [...], filters: {status, colors}, meaningOfLife: 42}
--- src/store.js
+++ src/store.js
@@ -1,18 +1,32 @@
 
-import {createStore} from 'redux'
+import {createStore, compose} from 'redux'
+import { includeMeaningOfLife, sayHiOnDispatch } from './exampleAddons/enhancers'
 import rootReducer from './reducer'
 
 //createStore can also accept a preloadedState value as its second argument. You could use this to add initial data when the store is created, such as values that were included in an HTML page sent from the server, or persisted in localStorage and read back when the user visits the page again, like this:
 
-let preloadedState
-const persistedTodosString = localStorage.getItem('todos')
+// let preloadedState
+// const persistedTodosString = localStorage.getItem('todos')
 
-if (persistedTodosString) {
-  preloadedState = {
-    todos: JSON.parse(persistedTodosString)
-  }
-}
+// if (persistedTodosString) {
+//   preloadedState = {
+//     todos: JSON.parse(persistedTodosString)
+//   }
+// }
 
-const store = createStore(rootReducer, preloadedState)
+// const store = createStore(rootReducer, preloadedState)
+
+// export default store;
+
+// const store = createStore(rootReducer, undefined, sayHiOnDispatch);
+// export default store;
+
+
+//createStore only accepts one enhancer as its third argument
+//  the Redux core includes a compose function that can be used to merge multiple enhancers together
+
+const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife);
+
+const store = createStore(rootReducer, undefined, composedEnhancer);
 
 export default store;
