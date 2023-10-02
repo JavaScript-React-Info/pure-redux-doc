7ab6b98998228db929ec66c1575eda828ac7a228
shubham V <Shubham.V@cerner.com>
2023-10-02 13:35:21 +0530

    example middleware

--- src/REDUX async/reducer.js
+++ src/REDUX async/reducer.js
@@ -2,6 +2,7 @@
 
 
 export const counterReducer = (state= 0, action)=>{
+    console.log("reducer ran to check when it will ran", action.type);
     switch(action.type){
         case 'counter/increment': {
             return state +1
--- src/REDUX async/store.js
+++ src/REDUX async/store.js
@@ -1,4 +1,28 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import store from '../store';
+import { decrement } from './action';
 import { counterReducer } from './reducer'
 
-export const asyncStore = createStore(counterReducer)
+// middleware catches the action before it hits the reducer
+const myLoggerMiddleWare = (storeAPI) => (next) => (action) => {
+    
+    return next(action); //this tell the app to pass the action to next middleware or reducer if this is last
+}
+
+const secondMiddleware = storeAPI => next => action => {
+   
+    return next(action);
+}
+
+const capAt10 =storeAPI => next => action => {
+   
+    if(storeAPI.getState() >= 10){
+        return next({type: 'counter/decrement'})
+    } else {
+       return next(action)
+    } 
+     
+     
+}
+
+export const asyncStore = createStore(counterReducer, applyMiddleware(myLoggerMiddleWare, secondMiddleware, capAt10))
